6c804288ceecd41ed0fb3c95befc9e7a3cedef72
Javier <caballerojavier13@users.noreply.github.com>
2015-03-14 11:36:59 -0300

    Update main.js

--- web/js/main.js
+++ web/js/main.js
@@ -1,9 +1,7 @@
 $(function(){
     $(window).load(function() {
-      setTimeout(function() {
         $("#content").show();
         $('#loader-wrapper ').hide();
-      }, 3000);
     });
     if( navigator.userAgent.match( /iPhone/i ) ) {
       $("#loader").addClass("loader-iphone");
