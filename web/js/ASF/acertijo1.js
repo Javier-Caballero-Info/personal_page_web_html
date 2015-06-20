92d4c9a96a9d28dee82899c1b1a8faea0511bb17
Javier Caballero <caballerojavier13@gmaill.com>
2015-06-20 02:18:04 -0300

    Segunda oportunidad en el acertijo 1

--- web/js/ASF/acertijo1.js
+++ web/js/ASF/acertijo1.js
@@ -5,75 +5,110 @@ var loop = 0;
 
 var worker;
 
+var run = 0;
+
 $(function(){
 
 });
 
 $("#predecir").on("click",function(e){
     e.preventDefault();
-    $("#predecir").html("Reiniciar Simulación");
     exitos = 0;
     perdidas = 0;
+    if(run > 0){
+      worker.terminate();
 
-    worker = new Worker('/js/ASF/worker_a1.js');
+      worker = undefined;
 
+      worker = new Worker('/js/ASF/worker_a1.js')
 
-    worker.onmessage = function(e) {
-      var p_juan = $("#p_juan").val();
-      var p_maria = $("#p_maria").val();
+      escucharWorker();
 
-      $($('#t_resutado1 tbody td')[0]).text(to_cara(p_juan));
-      $($('#t_resutado1 tbody td')[2]).text(to_cara(p_maria));
-      r_juan = e.data.r_juan;
-      r_maria = e.data.r_maria;
-      $($('#t_resutado1 tbody td')[1]).text(to_cara(r_juan));
+      worker.postMessage({speed: $("#speed").val()});
+    }else{
+      $("#predecir").html("Reiniciar Simulación");
+      $("#label_speed").html("Cambiar Velocidad:");
+      worker = new Worker('/js/ASF/worker_a1.js')
 
-      var ok = 0;
+      escucharWorker();
 
-      if(p_juan == r_juan){
-        $($('#t_resutado1 tbody tr')[0]).addClass("success");
-        $($('#t_resutado1 tbody tr')[0]).removeClass("danger");
-        ok = 1;
-      }else{
-        $($('#t_resutado1 tbody tr')[0]).addClass("danger");
-        $($('#t_resutado1 tbody tr')[0]).removeClass("success");
-      }
+      worker.postMessage({speed: $("#speed").val()});
 
-
-      $($('#t_resutado1 tbody td')[3]).text(to_cara(r_maria));
-
-      if(p_maria == r_maria){
-        $($('#t_resutado1 tbody tr')[1]).addClass("success");
-        $($('#t_resutado1 tbody tr')[1]).removeClass("danger");
-        ok = 1;
-      }else{
-        $($('#t_resutado1 tbody tr')[1]).addClass("danger");
-        $($('#t_resutado1 tbody tr')[1]).removeClass("success");
-      }
-
-      if(ok > 0){
-        exitos = exitos + 1;
-      }else{
-        perdidas = perdidas + 1;
-      }
-
-      $($('#t_total1 tbody td')[0]).text(exitos);
-      $($('#t_total1 tbody td')[1]).text(perdidas);
-
-
-      $($('#t_total1 tbody td')[2]).text(Math.round(parseFloat(parseFloat(exitos)/parseFloat(exitos + perdidas)) * 100) + '%');
-      $($('#t_total1 tbody td')[3]).text(Math.round(parseFloat(parseFloat(perdidas)/parseFloat(exitos + perdidas)) * 100) + '%');
-
-    };
-
-    worker.onerror = function(e) {
-      alert('Error: Line ' + e.lineno + ' in ' + e.filename + ': ' + e.message);
-    };
-
-
-    //start the worker
-    worker.postMessage({event: "run"});
+      run = 1;
+    }
 });
+
+$("#speed").on("change",function(){
+  if(run > 0){
+    worker.terminate();
+
+    worker = undefined;
+
+    worker = new Worker('/js/ASF/worker_a1.js')
+
+    escucharWorker();
+
+    worker.postMessage({speed: $("#speed").val()});
+  }
+});
+
+function escucharWorker(){
+  worker.onmessage = function(e) {
+
+    r_juan = e.data.r_juan;
+    r_maria = e.data.r_maria;
+
+    p_juan = opuesto(r_maria);
+
+    p_maria = r_juan;
+
+    $($('#t_resutado1 tbody td')[0]).text(to_cara(p_juan));
+    $($('#t_resutado1 tbody td')[2]).text(to_cara(p_maria));
+
+    $($('#t_resutado1 tbody td')[1]).text(to_cara(r_juan));
+
+    var ok = 0;
+
+    if(p_juan == r_juan){
+      $($('#t_resutado1 tbody tr')[0]).addClass("success");
+      $($('#t_resutado1 tbody tr')[0]).removeClass("danger");
+      ok = 1;
+    }else{
+      $($('#t_resutado1 tbody tr')[0]).addClass("danger");
+      $($('#t_resutado1 tbody tr')[0]).removeClass("success");
+    }
+
+
+    $($('#t_resutado1 tbody td')[3]).text(to_cara(r_maria));
+
+    if(p_maria == r_maria){
+      $($('#t_resutado1 tbody tr')[1]).addClass("success");
+      $($('#t_resutado1 tbody tr')[1]).removeClass("danger");
+      ok = 1;
+    }else{
+      $($('#t_resutado1 tbody tr')[1]).addClass("danger");
+      $($('#t_resutado1 tbody tr')[1]).removeClass("success");
+    }
+
+    if(ok > 0){
+      exitos = exitos + 1;
+    }else{
+      perdidas = perdidas + 1;
+    }
+
+    $($('#t_total1 tbody td')[0]).text(exitos);
+    $($('#t_total1 tbody td')[1]).text(perdidas);
+
+
+    $($('#t_total1 tbody td')[2]).text(Math.round(parseFloat(parseFloat(exitos)/parseFloat(exitos + perdidas)) * 100) + '%');
+    $($('#t_total1 tbody td')[3]).text(Math.round(parseFloat(parseFloat(perdidas)/parseFloat(exitos + perdidas)) * 100) + '%');
+
+  };
+
+  worker.onerror = function(e) {
+    alert('Error: Line ' + e.lineno + ' in ' + e.filename + ': ' + e.message);
+  };
+}
 
 function to_cara(valor){
   if(valor < 1){
@@ -81,4 +116,8 @@ function to_cara(valor){
   }else{
     return "Cruz";
   }
+}
+
+function opuesto(valor){
+  return (valor + 1) % 2;
 }
--- web/js/ASF/worker_a1.js
+++ web/js/ASF/worker_a1.js
@@ -1,20 +1,23 @@
-var exitos = 0;
-var perdidas = 0;
+function aleatorio(){
 
-var loop = 1;
-
-//function aleatorio(){
   //Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
-  //return Math.floor(Math.random() * (1 - 0 + 1)) + 0;
-//}
-
-
-function timedCount() {
-  var r_juan = Math.floor(Math.random() * (1 - 0 + 1)) + 0;
-  var r_maria = Math.floor(Math.random() * (1 - 0 + 1)) + 0;
-
-    postMessage({'r_juan': r_juan, 'r_maria': r_maria});
-    setTimeout("timedCount()",100);
+  return Math.floor(Math.random() * (1 - 0 + 1)) + 0;
 }
 
-timedCount();
+
+function tirarMonedas() {
+  var r_juan = aleatorio();
+  var r_maria = aleatorio();
+
+  //timedCount();
+  postMessage({'r_juan': r_juan, 'r_maria': r_maria});
+
+}
+
+//timedCount();
+
+
+onmessage = function (e) {
+  var speed = e.data.speed;
+  setInterval(tirarMonedas,speed);
+};
