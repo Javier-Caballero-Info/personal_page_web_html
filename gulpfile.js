09b3d4ed8c1a72636baf5d72ed12a0c0bb26733d
Javier Caballero <caballerojavier13@gmail.com>
2017-05-20 15:17:26 -0300

    Hasheo de los nombres de los assets para evitar cache cuando ocurre un cambio.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,29 @@
 var gulp = require('gulp');
+var through = require('through2')
 var concat = require('gulp-concat');
 var stripDebug = require('gulp-strip-debug');
 var uglify = require('gulp-uglify');
 var clean = require('gulp-clean');
 var concat = require('gulp-concat');
-var autoprefix = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
-var minifyHTML = require('gulp-minify-html');
+var htmlmin = require('gulp-htmlmin');
+var assetsHash = require('gulp-asset-hash');
+var batchReplace = require('gulp-batch-replace');
 var rm = require( 'gulp-rm' );
+var fs = require('fs');
 
 gulp.task('clean', ['clean:fonts','clean:css','clean:js','clean:img'], function() {});
 
-gulp.task('default', ['styles','fonts','scripts','img'], function() {});
+gulp.task('default', ['fonts', 'img', 'styles', 'scripts', 'html'], function() {});
+
+assetsHash.set({
+    manifest: './manifest-assets-hash.json',
+    length: 13,
+    hasher: 'md5'
+});
 
 var htmls = [
-'index.html'
+'src/index.html'
 ];
 /*
 
@@ -54,7 +63,7 @@ var fonts = [
 'src/fonts/**/*'
 ];
 
-var img = [
+var imgs = [
     'src/img/**/*'
 ];
 
@@ -84,20 +93,72 @@ gulp.task('fonts', function() {
 });
 
 gulp.task('img', function() {
-    gulp.src(img)
+    gulp.src(imgs)
+        .pipe(assetsHash.hash())
         .pipe(gulp.dest('./dist/img/'));
 });
 
 gulp.task('scripts', function() {
 	gulp.src(scripts)
         .pipe(concat('script.js'))
+        .pipe(assetsHash.hash())
         .pipe(gulp.dest('./dist/js'));
 });
 
 
 gulp.task('styles', function() {
+
+    var replaceAssets = [];
+
 	gulp.src(styles)
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(concat('style.css'))
+        .pipe(through.obj(function (chunk, enc, cb) {
+
+            var json_assets = JSON.parse(fs.readFileSync('./manifest-assets-hash.json'));
+
+            for(i in json_assets) {
+
+                var item = json_assets[i];
+
+                var original = item.original.split('/')[item.original.split('/').length -1];
+
+                var path = item.path.split('/')[item.path.split('/').length - 1];
+
+                replaceAssets.push([original, path]);
+            }
+
+            cb(null, chunk)
+        }))
+        .pipe(batchReplace(replaceAssets))
+        .pipe(assetsHash.hash())
         .pipe(gulp.dest('./dist/css'));
+});
+
+
+gulp.task('html', function() {
+
+    var replaceAssets = [];
+
+    gulp.src(htmls)
+        .pipe(through.obj(function (chunk, enc, cb) {
+
+            var json_assets = JSON.parse(fs.readFileSync('./manifest-assets-hash.json'));
+
+            for(i in json_assets) {
+
+                var item = json_assets[i];
+
+                var original = item.original.split('/')[item.original.split('/').length -1];
+
+                var path = item.path.split('/')[item.path.split('/').length - 1];
+
+                replaceAssets.push([original, path]);
+            }
+
+            cb(null, chunk)
+        }))
+        .pipe(batchReplace(replaceAssets))
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('./dist'));
 });
